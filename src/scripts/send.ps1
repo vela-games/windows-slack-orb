ffda115850fd9484f079304a04749be77cef8f7f
Illia Sukhariev <i.sukhariev@dragonslake.com>
2022-10-25 06:50:54 +0300

    change url to token

--- src/scripts/send.ps1
+++ src/scripts/send.ps1
@@ -1,6 +1,7 @@
-$body = ConvertTo-Json @{
-          pretext = "$env:SLACK_PARAM_TEXT"
-          text = "$env:SLACK_PARAM_MENTIONS"
-          channel = "$env:SLACK_PARAM_CHANNEL"
-        }
-Invoke-RestMethod -uri "$env:SLACK_ACCESS_TOKEN" -Method Post -body $body -ContentType 'application/json'
+$uriSlack = "https://slack.com/api/chat.postMessage"
+$headers = @{Authorization = "Bearer $env:SLACK_ACCESS_TOKEN"}
+$body = @{
+    text = "$env:SLACK_PARAM_TEXT `n<$env:SLACK_PARAM_MENTIONS>"
+    channel = "$env:SLACK_PARAM_CHANNEL"
+}
+Invoke-RestMethod -uri $uriSlack -Method Post -Headers $headers -body $body | Out-Null
