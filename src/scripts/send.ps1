a8062a6eb6e34b1a80d22ad9731f5337f2c40dd5
Illia Sukhariev <i.sukhariev@dragonslake.com>
2022-10-25 07:08:10 +0300

    fix mentions

--- src/scripts/send.ps1
+++ src/scripts/send.ps1
@@ -1,7 +1,7 @@
 $uriSlack = "https://slack.com/api/chat.postMessage"
 $headers = @{Authorization = "Bearer $env:SLACK_ACCESS_TOKEN"}
 $body = @{
-    text = "$env:SLACK_PARAM_TEXT `n<$env:SLACK_PARAM_MENTIONS>"
+    text = "$env:SLACK_PARAM_TEXT `n$env:SLACK_PARAM_MENTIONS"
     channel = "$env:SLACK_PARAM_CHANNEL"
 }
 Invoke-RestMethod -uri $uriSlack -Method Post -Headers $headers -body $body | Out-Null
