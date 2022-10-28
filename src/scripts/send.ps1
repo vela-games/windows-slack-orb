cb3ea891b13d52f2b4e3a5de30a398305ecceb9a
Illia Sukhariev <i.sukhariev@dragonslake.com>
2022-10-28 17:07:06 +0300

    v0.0.8 minor

--- src/scripts/send.ps1
+++ src/scripts/send.ps1
@@ -34,7 +34,7 @@ $headers = @{
     Authorization = "Bearer $env:SLACK_ACCESS_TOKEN"
 }
 $body = @{
-    text = "$env:SLACK_PARAM_TEXT `n$env:SLACK_PARAM_MENTIONS"
+    text = "$text `n$env:SLACK_PARAM_MENTIONS"
     channel = "$env:SLACK_PARAM_CHANNEL"
 }
 Invoke-RestMethod -uri $uriSlack -Method Post -Headers $headers -body $body | Out-Null
